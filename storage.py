3771b067c2c1c26204cfb8cedc5411f4b8ff8874
Andrey R <32875388+binrey@users.noreply.github.com>
2019-03-01 19:29:11 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -8,15 +8,14 @@ bucket_name = 'my_project_name.appspot.com'
 storage_client = storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
 
-NEW_WIDTH = 500
-
-def upload_file(upload_file, file_name):
+def upload_file(upload_file, file_name, new_width=500):
     """
     Upload file to the cloud storage
     Arguments: 
     upload_file -- binary file to be uploaded
     file_name -- name of file in clod storage
-
+    new_width -- new width of uploaded files
+    
     Returns:
     public_url -- http-path to stored file
     """
@@ -24,8 +23,8 @@ def upload_file(upload_file, file_name):
     blob = bucket.blob(file_name)
     pil_img = Image.open(upload_file)
 
-    NEW_HEIGHT = int(NEW_WIDTH/pil_img.size[0]*pil_img.size[1])
-    pil_img = pil_img.resize((NEW_WIDTH, NEW_HEIGHT))
+    new_height = int(new_width/pil_img.size[0]*pil_img.size[1])
+    pil_img = pil_img.resize((new_width, new_height))
 
     b = io.BytesIO()
     pil_img.save(b, 'jpeg')
